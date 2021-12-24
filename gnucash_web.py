5d51a93d6814547384b7e4da3d3dd11b7342d2c2
Joshua Bachmeier <joshua@bachmeier.cc>
2021-12-24 01:17:09 +0100

    Refactor account view

--- gnucash_web.py
+++ gnucash_web.py
@@ -3,6 +3,8 @@ from decimal import Decimal
 
 from flask import render_template, url_for, request, redirect, session
 from piecash import open_book, Transaction, Split
+from jinja2 import Environment, BaseLoader
+from babel import numbers
 
 from main import app, config, logger
 from auth import requires_auth, get_db_credentials
@@ -14,6 +16,24 @@ def parent_accounts(account):
 
 app.jinja_env.filters['parentaccounts'] = parent_accounts
 
+def money(amount, commodity):
+    if numbers.get_currency_symbol(commodity.mnemonic) != commodity.mnemonic:
+        value = numbers.format_currency(amount, commodity.mnemonic)
+    else:
+        value = f'{amount} {commodity.mnemonic}'
+
+    return Environment(loader=BaseLoader()).from_string('''
+      <span class="text-{% if amount >= 0 %}secondary{% else %}danger{% endif %}">
+        {{ value }}
+      </span>
+    ''').render(amount=amount, value=value)
+
+app.jinja_env.filters['money'] = money
+
+def account_url(account):
+    return url_for('show_account', account_name=account.fullname.replace(':', '/'))
+
+app.jinja_env.filters['accounturl'] = account_url
 
 @app.route('/accounts/<path:account_name>')
 @app.route('/accounts/', defaults={'account_name': ''})
