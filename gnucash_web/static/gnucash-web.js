a9fad123ad52697c1c08782979d5e590ab858aa9
Joshua Bachmeier <joshua@bachmeier.cc>
2022-09-27 00:18:12 +0200

    Implement transaction recycling button
    Resolves #14

--- gnucash_web/static/gnucash-web.js
+++ gnucash_web/static/gnucash-web.js
@@ -25,3 +25,9 @@ document.addEventListener("DOMContentLoaded", function(){
         $(this).toggleClass("bi-chevron-down");
     });
 });
+
+function transaction_recycle(description, value, postDate, contraAccounts) {
+    $("input[form=add_transaction][name=description]").attr("value", description);
+    $("input[form=add_transaction][name=value]").attr("value", value);
+    $("select[form=add_transaction][name=contra_account_name]")[0].selectize .addItem(contraAccounts[0]);
+}
