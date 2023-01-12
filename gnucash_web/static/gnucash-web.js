28af20545404881d7bd71e318837ee04642dba14
Joshua Bachmeier <joshua@bachmeier.cc>
2023-01-12 21:41:49 +0100

    Simplify account passing to transaction_recycle
    Only pass single account, we don't support multiple splits anyway

--- gnucash_web/static/gnucash-web.js
+++ gnucash_web/static/gnucash-web.js
@@ -52,8 +52,8 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-function transaction_recycle(description, value, postDate, contraAccounts) {
+function transaction_recycle(description, value, postDate, contraAccount) {
     $("input[form=new_transaction][name=description]").attr("value", description);
     $("input[form=new_transaction][name=value]").attr("value", value);
-    $("select[form=new_transaction][name=contra_account_name]")[0].selectize.addItem(contraAccounts[0]);
+    $("select[form=new_transaction][name=contra_account_name]")[0].selectize.addItem(contraAccount);
 }
